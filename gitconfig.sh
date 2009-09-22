c2c7f6d078f3d7cd3de55b68eefeb9e70b97518e
kn <root@greengrass.i2r.a-star.edu.sg>
2009-09-22 15:14:42 +0800

    1. init

---
+++ gitconfig.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+git config --global color.ui "auto"
+git config --global color.status auto
+git config --global color.branch auto
+git config --global color.diff auto
+git config --global color.interactive auto
+git config --global core.editor "vim"
+git config --global merge.tool meld
