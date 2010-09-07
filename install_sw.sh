41ea7a590d6d9bc2e93d812b704722026993237b
insidepower <insidepower@insidepower-laptop.(none)>
2010-09-08 00:21:53 +0800

    not using yankring, gksudo nautilus, install xsel

--- install_sw.sh
+++ install_sw.sh
@@ -1,12 +1,15 @@
 sudo apt-get -y install vim-gnome #(gtk is for xubuntu)
 sudo apt-get -y install build-essential
 sudo apt-get -y install exuberant-ctags
-sudo apt-get -y install xclip
+#sudo apt-get -y install xclip
+sudo apt-get -y install xsel
 sudo apt-get -y install git-doc
 sudo apt-get -y install gitk
 sudo apt-get -y install meld
-sudo apt-get -y install subversion
-sudo apt-get -y install git-svn
-sudo apt-get -y install ddd
-sudo apt-get -y install doxygen
+#sudo apt-get -y install subversion
+#sudo apt-get -y install git-svn
+#sudo apt-get -y install ddd
+#sudo apt-get -y install doxygen
 sudo apt-get -y install apparix
+sudo apt-get -y install speedcrunch
+#sudo apt-get -y install libncursesw5-dev
