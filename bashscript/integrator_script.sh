adfb7854298cd5fbf35f540c58ad4d7a0fb826ed
Ng Khian Nam <Khian.Nam.Ng@continental-corporation.com>
2010-09-16 16:16:35 +0800

    export ARCH and CROSS_COMPILE

--- bashscript/integrator_script.sh
+++ bashscript/integrator_script.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+export ARCH=arm
+export CROSS_COMPILE=/opt/gcc-4.1.2-glibc-2.5-nptl-3/arm-none-linux-gnueabi/bin/arm-none-linux-gnueabi-
+
 if [ $# -ge 1 ]; then
 	SUFFIX=nlev_soc_r${1}
 	echo "suffix = "$SUFFIX
